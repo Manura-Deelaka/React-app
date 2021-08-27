a3eb8fe0f986eaae7c80ecf16b1d199a3749e914
roma972012 <romesh.2017397@iit.ac.lk>
2021-08-28 03:13:16 +0530

    new changes added

--- src/components/_external-pages/About-us/ItemData.js
+++ src/components/_external-pages/About-us/ItemData.js
@@ -1,13 +1,14 @@
 const itemData = [
   {
-    img: '/static/home/apple.jpg',
+    img: '/static/home/kabul.jpg',
     title: 'කාබුල් හි ජ්‍යෙෂ්ඨ වෛද්‍ය නිලධාරියකු බීබීසී වෙත පැවසූ පරිදි පුද්ගලයින් 60 දෙනෙකුවත් මියගොස් තවත් 140 ක් පමණ තුවාල ලබා තිබේ.',
+    author: 'BBC',
     cols: 2,
     featured: true,
   },
   {
-    img: '/static/home/migraine.jpg',
-    title: 'ඇමෙරිකානු මරීන් භටයින් 11',
+    img: '/static/home/covid.jpg',
+    title: 'දින 5කට කොවිඩ් මරණ 1,000ක්',
     author: 'director90',
   },
   {
@@ -16,7 +17,7 @@ const itemData = [
     author: 'Danson67',
   },
   {
-    img: '/static/home/apple.jpg',
+    img: '/static/home/military.jpeg',
     title: 'ඇමෙරිකානු මරීන් භටයින් 11',
     author: 'fancycrave1',
     featured: true,
--- src/components/_external-pages/About-us/index.js
+++ src/components/_external-pages/About-us/index.js
@@ -1,1 +1,2 @@
 export { default as AboutHero } from './AboutHero';
+export { default as AboutMiddle } from './AboutMiddle';
